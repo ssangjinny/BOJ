a75f855b47653420e8ed93fe955055b261fddb33
ssangjinny <69946430+ssangjinny@users.noreply.github.com>
2020-11-09 09:44:38 +0900

    Update BOJ_2750.py

--- BOJ_2750.py
+++ BOJ_2750.py
@@ -1,8 +1,12 @@
 a = int(input())
-l = []
-for b in range(0, a):
-    l.append(int(input()))
+b = 3
+if a % 2 == 0 :
+    while b <= a//2:
+        b = b + 1
+        if a % b == 0:
+            count = count + 1
 
-l.sort()
-for b in range(0, a):
-    print(str(l[b]))
+else :
+    b = 2
+    while b <= a//2:
+        b
