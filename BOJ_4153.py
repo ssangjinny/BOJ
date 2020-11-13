200cd7d2067dba620ce3842e724e3356fa399469
ssangjinny <bsj3232@gmail.com>
2020-11-13 17:53:55 +0900

    Create BOJ_4153.py

---
+++ BOJ_4153.py
@@ -0,0 +1,9 @@
+while(True):
+    l = list(map(int, input().split()))
+    if sum(l) == 0:
+        break
+    l.sort()
+    if l[0]**2 + l[1]**2 == l[2]**2 :
+        print("right")
+    else :
+        print("wrong")
