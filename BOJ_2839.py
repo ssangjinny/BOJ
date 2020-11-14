98cbc2b75bf8c43ac79f7160bdf77068dfb6c75d
ssangjinny <69946430+ssangjinny@users.noreply.github.com>
2020-11-14 11:48:07 +0900

    Create BOJ_2839.py

---
+++ BOJ_2839.py
@@ -0,0 +1,24 @@
+N = int(input())
+a = N//5
+result = 0
+count = [0, 0]
+su = 0
+bu = 0
+if N % 5 == 0 :
+    result = N // 5
+    print(result)
+else:
+    for i in range(N//5, -1, -1):
+        count[0] = i
+        count[1] = 0
+        su = i * 5
+        while su < N:
+            su = su + 3
+            count[1] = count[1] + 1
+        if su == N:
+            bu = 1
+            break
+    result = sum(count)
+    if bu == 0:
+        result = -1
+    print(result)
